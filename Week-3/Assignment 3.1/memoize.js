c4d18ce32ce2b8b345b82c9c42b9ff84687bf557
Nilansh Audichya <nilansh.audichya18@gmail.com>
2022-08-22 22:31:39 +0530

    Assignment 3.1

---
+++ Week-3/Assignment 3.1/memoize.js
@@ -0,0 +1,30 @@
+// Function for adding two numbers
+function add(a,b){
+    return a+b ; 
+}
+//Custom memoize function
+function memoize(fn){
+    const cache= new Map();
+    return function( ...args){
+        const key= args.toString();
+        if (cache.has(key)){
+            return cache.get(key);
+        }
+        cache.set(key, fn(...args));
+        return cache.get(key);
+    };
+}
+
+// Function for time calculation
+function time(fn){
+    console.time();
+    fn();
+    console.timeEnd();
+}
+
+const memoizeAdd = memoize(add);
+
+time(() => memoizeAdd(100,100));
+time(() => memoizeAdd(100));
+time(() => memoizeAdd(100,200));
+time(() => memoizeAdd(100,100));
